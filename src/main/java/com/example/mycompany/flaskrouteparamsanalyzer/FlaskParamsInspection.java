008fce4778c95d51c32211296d16616dca142934
Anas302 <anas302.kh@gmail.com>
2025-03-26 01:20:29 +0100

    Implement LocalInspection for detecting if a route argument name is not equal to a view function parameter name.

--- src/main/java/com/example/mycompany/flaskrouteparamsanalyzer/FlaskParamsInspection.java
+++ src/main/java/com/example/mycompany/flaskrouteparamsanalyzer/FlaskParamsInspection.java
@@ -3,9 +3,15 @@ package com.example.mycompany.flaskrouteparamsanalyzer;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElementVisitor;
-import com.jetbrains.python.psi.PyElementVisitor;
-import com.jetbrains.python.psi.PyFunction;
+import com.jetbrains.python.psi.*;
+import com.jetbrains.rd.generator.nova.PredefinedType;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class FlaskParamsInspection extends LocalInspectionTool {
@@ -14,12 +20,54 @@ public class FlaskParamsInspection extends LocalInspectionTool {
         return new PyElementVisitor(){
             @Override
             public void visitPyFunction(@NotNull PyFunction function){
-                // logic
-                System.out.println("Visited a function");
-                holder.registerProblem(function,
-                        "Function args don't match route parameters",
-                        new FlaskParamsQuickFix(function.getParameterList(), null));
+                PyDecorator route = function.getDecoratorList().getDecorators()[0]; // change [0]
+                if (route == null)
+                    return;
+
+                PyExpression route_url = route.getArgumentList().getArguments()[0];
+                if (route_url == null) // e.g. app.route()
+                    return;
+
+                List<String> url_args = extractUrlParameters(route_url.getText());
+                if (url_args.isEmpty()) // e.g. app.route('/url/has/no/parameter')
+                    return;
+
+                PyParameter[] function_args = function.getParameterList().getParameters();
+                if (function_args.length == 0) // def view(): ...
+                    return;
+
+                if (function_args.length != url_args.size())
+                    return;
+
+                // check all arguments names in route match function parameters
+                for (int i = 0; i < function_args.length; i++) {
+                    String function_arg_name = function_args[i].getName();
+                    String url_arg_name = url_args.get(i);
+                    if (!function_arg_name.equals(url_arg_name))
+                        holder.registerProblem(function,
+                                "Function parameter " + function_arg_name + " don't match route parameter " + url_arg_name,
+                                new FlaskParamsQuickFix(function_args[i], url_arg_name));
+                }
             }
         };
     }
+
+    private List<String> extractUrlParameters(String url){
+        List<String> params = new ArrayList<>();
+        String regex = "<(.*?)>"; // Matches content inside < >
+        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(regex);
+        java.util.regex.Matcher matcher = pattern.matcher(url);
+
+        while (matcher.find())
+            params.add(matcher.group(1)); // Extract content inside < >
+
+        return params;
+    }
+
+    @Override
+    public @Nullable String getStaticDescription() {
+        return "This inspection checks if route arguments names in Flask match the function parameters.";
+    }
+
+
 }
--- src/main/java/com/example/mycompany/flaskrouteparamsanalyzer/FlaskParamsQuickFix.java
+++ src/main/java/com/example/mycompany/flaskrouteparamsanalyzer/FlaskParamsQuickFix.java
@@ -4,32 +4,47 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.codeInspection.util.IntentionName;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
-import com.jetbrains.python.psi.PyParameterList;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.psi.*;
 import org.jetbrains.annotations.NotNull;
 
 public class FlaskParamsQuickFix implements LocalQuickFix {
 
-    private final PyParameterList functionParams;
-    private final PyParameterList routeParams;
+    private final PyParameter functionParam;
+    private final String routeParamName;
 
-    public FlaskParamsQuickFix(PyParameterList functionParams, PyParameterList routeParams){
-        this.functionParams = functionParams;
-        this.routeParams = routeParams;
+    public FlaskParamsQuickFix(PyParameter functionParam, String routeParamName){
+        this.functionParam = functionParam;
+        this.routeParamName = routeParamName;
     }
 
     @Override
     public @IntentionName @NotNull String getName() {
-        return LocalQuickFix.super.getName();
+        return "Rename function parameter " + functionParam.getName() + " to " + routeParamName;
     }
 
     @Override
     public @IntentionFamilyName @NotNull String getFamilyName() {
-        return "";
+        return "Flask route parameters fixes";
     }
 
     @Override
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+        // element with the problem
+        PsiElement element = descriptor.getPsiElement(); // PyFunction
+        PyFunction function = (PyFunction) element;
 
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            PyParameter[] function_params = function.getParameterList().getParameters();
+            for (PyParameter param : function_params)
+                if (param == functionParam) {
+                    PyElementGenerator generator = PyElementGenerator.getInstance(project);
+                    PyParameter newParam = generator.createParameter(routeParamName);
+                    param.replace(newParam);
+                }
+        });
     }
 }
